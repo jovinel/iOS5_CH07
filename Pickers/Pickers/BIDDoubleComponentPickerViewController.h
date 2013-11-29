5b4115210ca4cd7264831662127960c290f4a37f
Jovi Nel <jnp.parilla@gmail.com>
2013-11-29 11:33:37 +0800

    Signed-off-by: Jovi Nel <jnp.parilla@gmail.com>

---
+++ Pickers/Pickers/BIDDoubleComponentPickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDDoubleComponentPickerViewController.h
+//  Pickers
+//
+//  Created by Maria Alice C. Lim on 11/29/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDDoubleComponentPickerViewController : UIViewController
+
+@end
