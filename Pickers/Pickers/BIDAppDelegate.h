b7009127c4803b183ae113f8ad5eb58ee4c2d839
Jovi Nel <jnp.parilla@gmail.com>
2013-11-29 11:34:12 +0800

    Created 5 sets of UIViewController
    Signed-off-by: Jovi Nel <jnp.parilla@gmail.com>

---
+++ Pickers/Pickers/BIDAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  BIDAppDelegate.h
+//  Pickers
+//
+//  Created by Maria Alice C. Lim on 11/29/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) IBOutlet UITabBarController *rootController;
+
+@end
