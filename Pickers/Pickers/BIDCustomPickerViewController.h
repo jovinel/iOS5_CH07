3c5177796fe90a96a683d442ba05a8a5d7aef741
Jovi Nel <jnp.parilla@gmail.com>
2013-11-29 16:46:43 +0800

    Added images for the app and implements methods
    Signed-off-by: Jovi Nel <jnp.parilla@gmail.com>

--- Pickers/Pickers/BIDCustomPickerViewController.h
+++ Pickers/Pickers/BIDCustomPickerViewController.h
@@ -8,6 +8,20 @@
 
 #import <UIKit/UIKit.h>
 
+
 @interface BIDCustomPickerViewController : UIViewController
+    <UIPickerViewDataSource, UIPickerViewDelegate>
+
+@property (strong, nonatomic) IBOutlet UIPickerView *picker;
+@property (strong, nonatomic) IBOutlet UILabel *winLabel;
+@property (strong, nonatomic) IBOutlet UIButton *button;
+@property (strong, nonatomic) NSArray *column1;
+@property (strong, nonatomic) NSArray *column2;
+@property (strong, nonatomic) NSArray *column3;
+@property (strong, nonatomic) NSArray *column4;
+@property (strong, nonatomic) NSArray *column5;
+
+
+- (IBAction)spin;
 
 @end
